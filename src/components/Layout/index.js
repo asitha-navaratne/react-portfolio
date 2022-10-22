9c95c5e9fa937c41ecab91305a9983fc8283c80c
Asitha Navaratne <asithan476@gmail.com>
2022-10-22 14:01:10 +0530

    Sidebar layout complete

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,10 @@
+import Sidebar from '../Sidebar'
 import './index.scss'
 
-const Layout = () => {
-  return <>Hello</>
-}
+const Layout = () => (
+  <>
+    <Sidebar />
+  </>
+)
 
 export default Layout
